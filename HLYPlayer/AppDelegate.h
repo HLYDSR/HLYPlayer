cfea7a94eb1e65664bb8a412c410e4307af8e38f
世如 <1224490668@qq.com>
2016-08-12 20:35:28 +0800

    Initial Commit

---
+++ HLYPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLYPlayer
+//
+//  Created by DengShiru on 16/8/12.
+//  Copyright © 2016年 hlyclub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLYPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLYPlayer
+//
+//  Created by DengShiru on 16/8/12.
+//  Copyright © 2016年 hlyclub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
